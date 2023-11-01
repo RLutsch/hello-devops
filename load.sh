0ba25eb54a308909dcc7973fc35cd93c464520b4
rlutsch <ross@pulse.tv>
2023-11-01 17:06:56 +0200

    echo steps to build so we can se what fails

--- load.sh
+++ load.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 # Check if there are changes in the Git repository
 if git diff --quiet; then
-    echo "No changes in the Git repository."
+    echo "No changes in the Git repository stopping here."
+    # Exit the script
+    exit 0
 else
-    # Pull the latest changes from the Git repository
+    echo "Pull the latest changes from the Git repository"
     git pull
 fi
-# Copy index.html to Nginx document root
+echo "Copy index.html to Nginx document root"
 sudo cp nginx/index.html /var/www/html/
 
-# Copy nginx.conf to Nginx configuration
+echo "Copy nginx.conf to Nginx configuration"
 sudo cp nginx/nginx.conf /etc/nginx/conf.d/nginx.conf
 
-# Restart Nginx
+echo "Restart Nginx"
 sudo systemctl restart nginx
 
-# Run Flask app on port 5000
+echo "Run Flask app on port 5000"
 cd python
 python3 app.py
