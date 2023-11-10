4b1a043adb52ea9640eb51990095d882c615ef71
rlutsch <ross@pulse.tv>
2023-11-10 11:03:53 +0200

    load.sh

--- load.sh
+++ load.sh
@@ -9,6 +9,6 @@ else
     echo "$(date +"%Y-%m-%d %H:%M:%S") No changes in the Git repository" >> /var/log/cron.log
     exit 0
 fi
+echo "docker-compose down and up"
 docker-compose down
-docker-compose build
-docker-compose up -d
+docker-compose up -d --build
